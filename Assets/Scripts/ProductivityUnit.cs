b9336428896d71b962820800d4330440f3b70d0f
Ibrahim Eren Kilisli <ierenkilisli@gmail.com>
2024-11-14 17:22:15 +0300

    new commit

---
+++ Assets/Scripts/ProductivityUnit.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ProductivityUnit : Unit
+{
+    protected override void BuildingInRange()
+    {
+        // Implement what should happen when the unit is in range of the building
+    }
+}
