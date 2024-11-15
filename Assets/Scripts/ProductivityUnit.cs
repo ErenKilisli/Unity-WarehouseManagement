28828c0eba0ae2c6031e375e50a805fff0d99cc5
Ibrahim Eren Kilisli <ierenkilisli@gmail.com>
2024-11-15 12:36:08 +0300

    pre optimization oob

--- Assets/Scripts/ProductivityUnit.cs
+++ Assets/Scripts/ProductivityUnit.cs
@@ -2,8 +2,42 @@ using UnityEngine;
 
 public class ProductivityUnit : Unit
 {
+    private ResourcePile m_CurrentPile = null;
+    public float ProductivityMultiplier = 2;
+
     protected override void BuildingInRange()
     {
-        // Implement what should happen when the unit is in range of the building
+        if (m_CurrentPile == null)
+        {
+            ResourcePile pile = m_Target as ResourcePile;
+
+            if (pile != null)
+            {
+                m_CurrentPile = pile;
+                m_CurrentPile.ProductionSpeed *=  ProductivityMultiplier;
+            }
+        }
     }
+
+    void ResetProductivity()
+    {
+        if (m_CurrentPile != null)
+        {
+            m_CurrentPile.ProductionSpeed /= ProductivityMultiplier;
+            m_CurrentPile = null;
+        }
+    }
+
+    public virtual void GoTo(Building target)
+    {
+        ResetProductivity(); // call your new method
+        base.GoTo(target); // run method from base class
+    }
+
+    public virtual void GoTo(Vector3 position)
+    {
+        ResetProductivity();
+        base.GoTo(position);
+    }
+
 }
